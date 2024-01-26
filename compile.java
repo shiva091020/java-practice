cf01b4af7a55c072043217b72e084a9d52ede66c
shiva091020 <shekarshiva02@gmail.com>
2024-01-26 13:06:39 +0530

    addition

--- compile.java
+++ compile.java
@@ -1,5 +1,20 @@
-class compile{
-  public static void main(String[] args) {
-    System.out.println("exicute program");
-  }
-
+<html>
+	<head>
+		<!-- head has only 5 tags -->
+		<!-- title,meta,style,link,scriipt -->
+		<title>First WebSite | Home Page </title>
+	</head>
+	<body>
+		<img src="./logo.png"/>
+		<a href="./index.html">Home</a>
+		<a href="./about.html">About</a>
+		<a href="./services.html">Services</a>
+		<a href="./employees.html">Employees</a>
+		<a href="./contact.html">Contact</a>
+		<a href="./login.html">Login</a>
+		<hr/>
+		<div>
+			<h1>About Page</h1>
+		</div>
+	</body>
+</html>
--- learn.java
+++ learn.java
@@ -1,5 +1,9 @@
-class learn java{
+class learnjava{
   public static void main(String[] args) {
+	int a=26;
+	int b=-6;
+	System.out.println("c equal is  "+(a+b));
     System.out.println("practice java");
   }
 
+}
