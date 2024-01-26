b3eff6e76da865eb3c73a9bd4fd3770e87854e59
shiva091020 <shekarshiva02@gmail.com>
2024-01-26 13:35:16 +0530

    substraction

--- learn.java
+++ learn.java
@@ -1,9 +1,19 @@
 class learnjava{
   public static void main(String[] args) {
-	int a=26;
-	int b=-6;
+	int a=2147483646;
+	int b=2;
 	System.out.println("c equal is  "+(a+b));
     System.out.println("practice java");
   }
 
 }
+class readjava{
+  public static void main(String[] args) {
+	int c=1257;
+	int d=450;
+	int e=-20;
+	System.out.println(c-d-e);
+
+   }
+}
+ 
