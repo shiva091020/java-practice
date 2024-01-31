0925cb5bba869ff42053bb83066b63b7903ee396
shiva091020 <shekarshiva02@gmail.com>
2024-01-31 14:11:50 +0530

    array

--- Test2.java
+++ Test2.java
@@ -1,5 +1,5 @@
 
-class shiva{
+class Test2{
    public static void mian(String[] args) {
      int[] ids={101,102,103,109,108,107};
      System.out.println(ids); 
---
+++ Test3.java
@@ -0,0 +1,11 @@
+class array {
+   public static void main(String[] args){
+     int[] ids={101,102,103,109,108,107};
+     System.out.println(ids[0]);
+      System.out.println(ids[1]);
+       System.out.println(ids[2]);
+       System.out.println(ids[3]);
+        System.out.println(ids[4]);
+       System.out.println(ids[5]);  
+  } 
+}
