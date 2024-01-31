656fe9ef2a43fe93a2eacbb29dcc1c5a807b543f
shiva091020 <shekarshiva02@gmail.com>
2024-01-31 15:38:12 +0530

    String

--- Test3.java
+++ Test3.java
@@ -1,10 +1,12 @@
 class array {
    public static void main(String[] args){
-    get_details (new int[] {101,102,103,109,108,107});
-    }
-        public static void get_details(int[] ids){
- System.out.println(ids[0]);
-	
-System.out.println(ids.length);
+     String ename4="shiva";
+     String ename3="sai";
+     String ename2="raj";
+     String ename1="bangalore";
+	System.out.println(ename1);
+        System.out.println(ename2);
+	System.out.println(ename3);
+	System.out.println(ename4); 
      }
 }
