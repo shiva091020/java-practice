0aac354b0b8918294be47e7c5708e37e126d579d
shiva091020 <shekarshiva02@gmail.com>
2024-01-31 14:27:00 +0530

    array

--- Test3.java
+++ Test3.java
@@ -1,11 +1,10 @@
 class array {
    public static void main(String[] args){
-     int[] ids={101,102,103,109,108,107};
-     System.out.println(ids[0]);
-      System.out.println(ids[1]);
-       System.out.println(ids[2]);
-       System.out.println(ids[3]);
-        System.out.println(ids[4]);
-       System.out.println(ids[5]);  
-  } 
+    get_details (new int[] {101,102,103,109,108,107});
+    }
+        public static void get_details(int[] ids){
+ System.out.println(ids[0]);
+	
+System.out.println(ids.length);
+     }
 }
