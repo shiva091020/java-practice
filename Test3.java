197196f1e7019e2e0208134b66ec3dce4563988d
shiva091020 <shekarshiva02@gmail.com>
2024-01-31 16:06:26 +0530

    ename

--- Test3.java
+++ Test3.java
@@ -1,12 +1,9 @@
 class array {
    public static void main(String[] args){
-     String ename4="shiva";
-     String ename3="sai";
-     String ename2="raj";
-     String ename1="bangalore";
-	System.out.println(ename1);
-        System.out.println(ename2);
-	System.out.println(ename3);
-	System.out.println(ename4); 
+     String[] enames={"shiva","raj","sai","kanna"};
+	System.out.println(enames[0]);
+        System.out.println(enames[1]);
+	System.out.println(enames[2]);
+	System.out.println(enames[3]); 
      }
 }
