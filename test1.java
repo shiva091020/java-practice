a9c88372c30d17f21740ada66264b4e819d2f1fc
shiva091020 <shekarshiva02@gmail.com>
2024-01-31 13:00:57 +0530

    java

---
+++ test1.java
@@ -0,0 +1,14 @@
+class java{
+  public static void main(String[] orgs){
+     int a=101;
+     int b=102;
+     int c=103;
+     int d=104;
+     int e=105;
+  System.out.println(a);
+System.out.println(b);   
+System.out.println(c);   
+System.out.println(d);   
+System.out.println(e);          
+  }
+}
