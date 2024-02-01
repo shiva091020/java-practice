fbe580fd5c11ad464a2dbf9c42ae7cd293a994ae
shiva091020 <shekarshiva02@gmail.com>
2024-02-01 13:17:34 +0530

    array2

--- Test3.java
+++ Test3.java
@@ -1,9 +1,13 @@
 class array {
    public static void main(String[] args){
-     String[] enames={"shiva","raj","sai","kanna"};
+     get_details (new String[]{"shiva","raj","sai","kanna"});
+     }
+   public static void get_details(String[] enames){
+	
 	System.out.println(enames[0]);
-        System.out.println(enames[1]);
+       System.out.println(enames[1]);
 	System.out.println(enames[2]);
 	System.out.println(enames[3]); 
      }
+
 }
