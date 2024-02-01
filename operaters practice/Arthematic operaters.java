7afd8bb4fbee96ca5c2eb8976945381a2ca73d85
shiva091020 <shekarshiva02@gmail.com>
2024-02-01 16:15:18 +0530

    add,sub,multiple,divide,modulus

---
+++ operaters practice/Arthematic operaters.java
@@ -0,0 +1,9 @@
+class arithematic{
+ public static void main(Strings[] orgs){
+       int a=35;
+       int b=55;
+       int c=?;
+	System.out.println(c); 	 	
+   }
+}
+    
---
+++ operaters practice/arthematic.java
@@ -0,0 +1,42 @@
+class arithematic{
+ public static void main(String[] orgs) {
+       int a=35;
+       int b=55;
+       int c=a+b;
+	System.out.println(c); 	 	
+   }
+}
+class arithematic1{
+   public static void main(String[] orgs){
+      int c=-369;
+      int d=-526;
+      int e=c-d;
+	System.out.println(e);	
+   }
+}
+class arithematic2{
+   public static void main(String[] orgs){
+       int e=458;
+       int f=1456;
+       int g=e*f;
+    	System.out.println(g);
+   }
+}
+class arithematic3{
+   public static void main(String[] orgs){
+    int g=2963;
+    int h=2365;
+    int i=g/h;
+      System.out.println(i);
+   }
+}
+class arithematic4{
+   public static void main(String[] orgs){
+     int i=20;
+     int j=10;
+     int k=i%j;
+       System.out.println(k);
+   }
+}
+
+    
