00ae75b39dd54b1c75d52db29ddf700c53d6e879
shiva091020 <shekarshiva02@gmail.com>
2024-02-06 11:52:14 +0530

    condition

---
+++ conditional/Evenodd.java
@@ -0,0 +1,10 @@
+class Test{
+public static void main(String[] args){
+int i=4245567;
+if(i%2==0){
+System.out.println("The given number is even");
+}
+else
+System.out.println("The given number is odd");
+}
+}
---
+++ conditional/greatest.java
@@ -0,0 +1,11 @@
+class Test{
+public static void main(String[] args){
+int a=3035454;
+int b=2045567;
+if(b<=a){
+System.out.println(b);
+}
+else
+System.out.println(a);
+}
+}
