eb92dcf905d7586c9fce9594ba3be8047372f5fa
shiva091020 <shekarshiva02@gmail.com>
2024-03-02 18:28:00 +0530

    singlelevel inheritance

---
+++ Access modifiers/classes/singlelevel.java
@@ -0,0 +1,12 @@
+class parent{
+  public void m1(){
+   System.out.println("parent class m1 method");  
+  }
+}
+class child extends parent{
+   public static void main(String[] orgs){
+     System.out.println("child class main method");
+	  parent p1=new parent();
+	  p1.m1();
+   }
+}
