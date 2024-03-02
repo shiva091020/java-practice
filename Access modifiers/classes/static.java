f794f67e26ca8e65e4b3239bb9be8b9d55759892
shiva091020 <shekarshiva02@gmail.com>
2024-03-02 13:37:12 +0530

    static method

---
+++ Access modifiers/classes/static.java
@@ -0,0 +1,14 @@
+class test{
+	static int c =999;
+     static void m1(){
+	
+			 
+	 }	 
+}
+ class test1 extends test{
+  public static void main(String[] orgs){
+	 test t1=new test();
+	 t1.m1();
+	System.out.println(t1.c);
+	 }
+}
