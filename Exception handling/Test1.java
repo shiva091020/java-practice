74af4a7c9f80a0123053150353a6e38b21e1661b
shiva091020 <shekarshiva02@gmail.com>
2024-03-11 16:11:50 +0530

    AE and try&catch method

---
+++ Exception handling/Test1.java
@@ -0,0 +1,12 @@
+class test{     
+  public static void main(String[] args){
+   System.out.println("shiva");
+   try{
+   System.out.println(10/0);
+   }
+   catch(Exception e){
+   System.out.println(10/2);
+  }
+   System.out.println("fouqiya");
+  }
+}   
---
+++ Exception handling/test.java
@@ -0,0 +1,7 @@
+class test{      //arithematic exception handling
+  public static void main(String[] args){
+   System.out.println("shiva");
+   System.out.println(10/0);
+   System.out.println("fouqiya");
+  }
+}   
