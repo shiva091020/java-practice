2db30a95c4d75eb4bc085b650d706774adaff67c
shiva091020 <shekarshiva02@gmail.com>
2024-03-27 15:40:47 +0530

    wrapper classes

---
+++ wrapper cls/five.java
@@ -0,0 +1,8 @@
+class Test{
+	public static void main(String[] args){
+		 String b="900";
+         int a=Integer.parseInt(b);
+          System.out.println(a);
+         System.out.println(b);
+	}
+}
