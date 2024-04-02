f042ebe117dd1e16de636282d6958d3bd5b28342
shiva091020 <shekarshiva02@gmail.com>
2024-04-02 16:22:07 +0530

    collection F/W

---
+++ collection framework/Array.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList; 
+class Test{
+   public static void main(String[] args){
+     ArrayList al= new ArrayList();
+	   al.add(929);
+	   al.add(299);
+	   System.out.println(al);
+	   System.out.println(al.get(0));
+	   System.out.println(al.get(1));
+   }
+}
---
+++ collection framework/add.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class Test{
+   public static void main(String[] args){
+       ArrayList al=new ArrayList();
+	   al.add("shiva");
+	   al.add(9);
+	   al.add("sai");
+	   al.add(007);
+	   System.out.println(al);
+   }
+}
